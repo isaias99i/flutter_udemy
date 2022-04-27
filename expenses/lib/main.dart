84f3e074f7d5bd918da04b17bebb56b88b935050
isaias99i <isaiascarlos99i@gmail.com>
2022-04-26 23:03:30 -0300

    Alteração de Stateless para Stateful e início de inserção de temas

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,25 +1,44 @@
-import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-
 import 'dart:math';
 import './models/transaction.dart';
 import './components/transaction_form.dart';
 import './components/transaction_list.dart';
 
-
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-    @override
+  @override
   Widget build(BuildContext context) {
+    final ThemeData tema = ThemeData();
+
     return MaterialApp(
       home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.redAccent,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -27,65 +46,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _transactions = [
     Transaction(
-        id: 't1', title: 'Whey Protein 2Kg', value: 99.99, date: DateTime.now()),
+        id: 't1',
+        title: 'Whey Protein 2Kg',
+        value: 99.99,
+        date: DateTime.now()),
     Transaction(
-        id: 't2', title: 'Creatina - 500g', value: 232.20, date: DateTime.now()),
+        id: 't2',
+        title: 'Creatina - 500g',
+        value: 232.20,
+        date: DateTime.now()),
   ];
 
-  _addTransaction(String title, double value){
+  _addTransaction(String title, double value) {
     final newTransaction = Transaction(
         id: Random().nextDouble().toString(),
         title: title,
         value: value,
-        date: DateTime.now()
-    );
+        date: DateTime.now());
 
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
-  _openTransactionFormModal(BuildContext context){
+  _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
         context: context,
-        builder: (_){
-          return TransactionForm(null!);
-        }
-    );
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _openTransactionFormModal(context),
-            )
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionList(_transactions),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Gráfico'),
-                  elevation: 5,
-                ),
-              ),
-              TransactionList(_transactions),
-              Column(
-        children: [
-          TransactionForm(_addTransaction),
-        ],
-      )
-            ],
-          ),
-        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _openTransactionFormModal(context),
@@ -93,4 +113,6 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+
+  void onSubmit(String p1, double p2) {}
 }
